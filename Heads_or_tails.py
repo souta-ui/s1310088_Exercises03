925e328118f3232ca818a80234049067bff07927
渡部颯太 <watanabesouta@wlan-napt-002.u-aizu.ac.jp>
2024-06-28 09:47:04 +0900

    Victory message added

--- Heads_or_tails.py
+++ Heads_or_tails.py
@@ -11,3 +11,8 @@ for i, result in enumerate(results, 1):
 heads_count = results.count("Heads")
 tails_count = results.count("Tails")
 print(f"Heads: {heads_count}, Tails: {tails_count}")
+
+if heads_count > tails_count:
+    print("You won!")
+else:
+    print("You lost!")
